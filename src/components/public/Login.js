323ba82897af0a66382673312b8524bfb0f2a1f4
Ferenc Antal <af@dorsum.eu>
2024-02-17 14:50:44 +0100

    Auth expand with axios

--- src/components/public/Login.js
+++ src/components/public/Login.js
@@ -1,28 +1,69 @@
 import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../../context/AuthProvider";
 
+import axios from "../../api/axios";
+const LOGIN_URL = "/login";
+
+//console.log(LOGIN_URL);
 export function Login() {
-  const userRef = useRef();
+  const { setAuth } = useContext(AuthContext);
+  const userEmailRef = useRef();
   const errRef = useRef();
 
-  const [user, setUser] = useState("");
-  const [pwd, setPwd] = useState("");
+  const [email, setUserEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
 
+  console.log(email, password);
+
   useEffect(() => {
-    userRef.current.focus();
+    userEmailRef.current.focus();
   }, []);
 
   useEffect(() => {
     setErrMsg("");
-  }, [user, pwd]);
+  }, [email, password]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user, pwd);
-    setUser("");
-    setPwd("");
-    setSuccess(true);
+    try {
+      // console.log(email, password);
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ email, password }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+        
+      );
+      console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth(email, password, roles, accessToken);
+      setUserEmail("");
+      setPassword("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Invalid Username or Password");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else if (err.response?.status === 500) {
+        setErrMsg("Internal Server Error");
+      } else if (err.response?.status === 405) {
+        setErrMsg("Method Not Allowed");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
   };
 
   return (
@@ -46,14 +87,14 @@ export function Login() {
           </p>
           <h1>Sign In</h1>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
+            <label htmlFor="email">Email:</label>
             <input
               type="text"
-              id="username"
-              ref={userRef}
+              id="email"
+              ref={userEmailRef}
               autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
+              onChange={(e) => setUserEmail(e.target.value)}
+              value={email}
               required
             />
 
@@ -61,8 +102,8 @@ export function Login() {
             <input
               type="password"
               id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               required
             />
             <button>Sign In</button>
