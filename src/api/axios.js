143fd1effcf2f64705a486f0b1597b1ec10e36ea
Ferenc Antal <af@dorsum.eu>
2024-03-02 17:11:07 +0100

    useAuth bevezetése axios refactorálás

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,17 @@
 import axios from "axios";
+const BASE_URL = "http://localhost:8000/api/staff";
+
 export default axios.create({
-  baseURL: "http://localhost:8000/api/staff",
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    withCredentials: false,
+  },
+});
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    withCredentials: true,
+  },
 });
